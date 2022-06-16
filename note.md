9a7da7cd371a897b668f1979301f7920d5492ba6
Leo Sun <sundalei1988@gmail.com>
2022-06-16 11:39:31 +0800

    add method security

---
+++ note.md
@@ -0,0 +1,1 @@
+https://github.com/spring-projects/spring-security/issues/5341
--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,6 +14,7 @@ import tacos.data.UserRepository;
 import tacos.domain.TacoUser;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
