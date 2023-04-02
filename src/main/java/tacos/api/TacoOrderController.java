4828fb275cfbb0b7154d080251db95862b58b134
Leo Sun <leosun@Leos-MacBook-Pro.local>
2023-04-02 16:31:38 +0800

    taco order rest api

--- src/main/java/tacos/api/TacoOrderController.java
+++ src/main/java/tacos/api/TacoOrderController.java
@@ -4,11 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tacos.data.OrderRepository;
 import tacos.domain.TacoOrder;
 import tacos.domain.TacoUser;
@@ -16,6 +16,7 @@ import tacos.web.OrderProps;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/orders")
@@ -30,7 +31,7 @@ public record TacoOrderController(OrderRepository orderRepository, OrderProps or
 
     @GetMapping("/deliveryZip/{deliveryZip}/startDate/{startDate}/endDate/{endDate}")
     public List<TacoOrder> findOrdersByDeliveryZipAndDate(@PathVariable String deliveryZip,
-            @PathVariable Date startDate, @PathVariable Date endDate) {
+                                                          @PathVariable Date startDate, @PathVariable Date endDate) {
         return orderRepository.readOrdersByDeliveryZipAndPlacedAtBetween(deliveryZip, startDate, endDate);
     }
 
@@ -42,4 +43,64 @@ public record TacoOrderController(OrderRepository orderRepository, OrderProps or
         return orderRepository.findByUserOrderByPlacedAtDesc(user, pageable);
     }
 
+    @GetMapping(path = "/{orderId}")
+    public ResponseEntity<TacoOrder> fetchOrder(@PathVariable Long orderId) {
+        Optional<TacoOrder> optOrder = orderRepository.findById(orderId);
+
+        return optOrder.map(tacoOrder -> ResponseEntity.ok().body(tacoOrder))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping(path = "/{orderId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public TacoOrder putOrder(@PathVariable Long orderId, @RequestBody TacoOrder order) {
+        order.setId(orderId);
+        return orderRepository.save(order);
+    }
+
+    @PatchMapping(path = "/{orderId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public TacoOrder patchOrder(@PathVariable Long orderId, @RequestBody TacoOrder patch) {
+
+        TacoOrder order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new RuntimeException("No order with id " + orderId + " exists."));
+
+        if (patch.getDeliveryName() != null) {
+            order.setDeliveryName(patch.getDeliveryName());
+        }
+
+        if (patch.getDeliveryStreet() != null) {
+            order.setDeliveryStreet(patch.getDeliveryStreet());
+        }
+
+        if (patch.getDeliveryCity() != null) {
+            order.setDeliveryCity(patch.getDeliveryCity());
+        }
+
+        if (patch.getDeliveryState() != null) {
+            order.setDeliveryState(patch.getDeliveryState());
+        }
+
+        if (patch.getDeliveryZip() != null) {
+            order.setDeliveryZip(patch.getDeliveryZip());
+        }
+
+        if (patch.getCcNumber() != null) {
+            order.setCcNumber(patch.getCcNumber());
+        }
+
+        if (patch.getCcExpiration() != null) {
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+
+        if (patch.getCcCVV() != null) {
+            order.setCcCVV(patch.getCcCVV());
+        }
+
+        return orderRepository.save(order);
+    }
+
+    @DeleteMapping("/{orderId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteOrder(@PathVariable Long orderId) {
+        orderRepository.deleteById(orderId);
+    }
 }
