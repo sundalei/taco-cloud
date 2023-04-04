e1734bb2bee483b877acc8f35a9b527b45732141
sun dalei <sundalei1988@gmail.com>
2023-04-04 15:20:13 +0800

    refactor

--- src/main/java/tacos/domain/Taco.java
+++ src/main/java/tacos/domain/Taco.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,7 +16,6 @@ import jakarta.validation.constraints.Size;
 
 @Data
 @Entity
-@RepositoryRestResource(itemResourceRel = "tacos", path = "tacos")
 public class Taco {
 
     @Id
